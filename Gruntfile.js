d3a118c43508aa987400766bb40a6af61b43fe8c
Ringen <lisa.ringen.ring@gmail.com>
2015-01-11 21:32:56 +0100

    Concatenates scss files into main.scss. Builds css and concatenated js to /build. Do this with defauld grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,42 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: '\n\n'
-      },
-      build: {
-        src: ['src/javascripts/carousel.js'],
-		dest: 'public_src/javascripts/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: '\n\n'
+			},
+			build: {
+				src: ['src/javascripts/*.js'],
+				dest: 'src/build/javascripts/<%= pkg.name %>.min.js'
+			}
+		},
+		sass: {
+			dist: {
+				files: {
+					'src/build/stylesheets/style.css' : 'src/stylesheets/main.scss'
+				},
+				options: {
+					style: 'nested',
+					sourcemap: false
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-concat');
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat']);
-
+	// Default task(s).
+	grunt.registerTask('default', ['sass', 'concat']);
+	grunt.registerTask('watch', ['watch']);
 };
