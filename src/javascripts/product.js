e5a8019804fab40561b0d6f6b66ea55b2b1d6d04
Ringen <lisa.ringen.ring@gmail.com>
2015-01-24 23:42:39 +0100

    Created product page

--- src/javascripts/product.js
+++ src/javascripts/product.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 
-	$('.a11y_js-product').hover(
+	$('.a11y_js-product-item').hover(
 		//mouseenter
 		function () {
-			var expandableSection = $(this).find('.a11y_js-product__expandable-section');
+			var expandableSection = $(this).find('.a11y_js-product-item__expandable-section');
 			expandableSection.addClass('is-expanded');
 		},
 		//mouseleave
 		function () {
-			var expandableSection = $(this).find('.a11y_js-product__expandable-section');
+			var expandableSection = $(this).find('.a11y_js-product-item__expandable-section');
 			expandableSection.removeClass('is-expanded');
 		}
 	);
 
-	$('.a11y_js-product').focus(
+	$('.a11y_js-product-item').focus(
 		//mouseenter
 		function () {
-			var expandableSection = $(this).find('.a11y_js-product__expandable-section');
+			var expandableSection = $(this).find('.a11y_js-product-item__expandable-section');
 			expandableSection.addClass('is-expanded');
 		},
 		//mouseleave
 		function () {
-			var expandableSection = $(this).find('.a11y_js-product__expandable-section');
+			var expandableSection = $(this).find('.a11y_js-product-item__expandable-section');
 			expandableSection.removeClass('is-expanded');
 		}
 	);
